e7826c826e534410e6b8c716e5684c119f769a51
Billy Campbell <wcampbell91@gmail.com>
2021-01-19 20:41:59 -0600

    upload image / ratings works!

--- raterapi/models/game_picture.py
+++ raterapi/models/game_picture.py
@@ -3,7 +3,7 @@ from django.db.models.deletion import CASCADE
 from django.db.models.fields import related
 from .game import Game
 
-class GamePicture:
+class GamePicture(models.Model):
     game = models.ForeignKey(Game, on_delete=models.DO_NOTHING, related_name="pictures")
     action_pic = models.ImageField(upload_to='actionimages', height_field=None, 
     width_field=None, max_length=None, null=True)
